1bfe9d88d0419fed4f02a8d25ccc314d07a31b81
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-08-07 11:16:25 +0530

    Updates....

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
-// Root of the app
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Sample',
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: HomeScreen(),
-    );
-  }
-}
-
-// StatefulWidget to manage state
-class HomeScreen extends StatefulWidget {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int counter = 0;
-  final nameController = TextEditingController();
-
-  void incrementCounter() {
-    setState(() {
-      counter++;
-    });
-  }
-
-  void showNameDialog() {
-    showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Text("Hello!"),
-        content: Text("Your name is ${nameController.text}"),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("OK"),
-          )
-        ],
+      debugShowCheckedModeBanner: false,
+      
+      title: 'Flutter Demo',
+      
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter Widgets Demo")),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(child: Text("Welcome!", style: TextStyle(fontSize: 24))),
-            ListTile(title: Text("Option 1")),
-            ListTile(title: Text("Option 2")),
-          ],
+      
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World App'),
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            // Profile Card
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.purple[100],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                children: [
-                  Icon(Icons.person, size: 48),
-                  SizedBox(width: 12),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Priyanshu Yadav", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                      Text("Flutter Learner"),
-                    ],
-                  )
-                ],
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            // Counter Section
-            Text("Button clicked $counter times", style: TextStyle(fontSize: 18)),
-            ElevatedButton(
-              onPressed: incrementCounter,
-              child: Text("Increment"),
-            ),
-
-            SizedBox(height: 20),
-
-            // Input Form
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                labelText: "Enter your name",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: showNameDialog,
-              child: Text("Submit"),
-            ),
-          ],
+        
+        body: Center(
+          child: Text('Hello, World!'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        child: Icon(Icons.add),
       ),
     );
   }
